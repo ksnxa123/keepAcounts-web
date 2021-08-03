ab1086165255d7c0be57aaab958430afab9dc3fe
xa <2387052992@qq.com>
2021-08-03 13:08:52 +0800

    deploy

---
+++ precache-manifest.fa0901b8c6b9d8bc04d662652ec16baf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5d9e3066c6d9047de5",
+    "url": "/keepAcounts-web/css/app.3f85579e.css"
+  },
+  {
+    "revision": "2f3eb011e754aa6d1040",
+    "url": "/keepAcounts-web/css/chunk-vendors.c3cdfcb7.css"
+  },
+  {
+    "revision": "ed988c502fcc2a93c6420c05a3966b1c",
+    "url": "/keepAcounts-web/index.html"
+  },
+  {
+    "revision": "3f5d9e3066c6d9047de5",
+    "url": "/keepAcounts-web/js/app.c9f9a59d.js"
+  },
+  {
+    "revision": "2f3eb011e754aa6d1040",
+    "url": "/keepAcounts-web/js/chunk-vendors.6305959e.js"
+  },
+  {
+    "revision": "9a157e3298bcac472798ba0beba2867d",
+    "url": "/keepAcounts-web/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/keepAcounts-web/robots.txt"
+  }
+]);
